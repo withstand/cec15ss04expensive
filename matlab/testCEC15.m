77c840396d706fa2a18443f4e0973369deea1b4d
withstand <chenqin1980@gmail.com>
2014-10-09 12:07:28 +0100

    Fix a bug with the JVM File.mkdirs problem of Matlab 7.12.0.635

--- matlab/testCEC15.m
+++ matlab/testCEC15.m
@@ -1,6 +1,6 @@
 
 
-prefix = 'result/201410091125/test_run';
+prefix = 'results\pattersearch\resultfile';
 % with output filel prefix,
 % canbe dir\\filenameprefix or dir/filenameprefix
 prob = cec2015.CEC15Problems(prefix);
@@ -12,7 +12,7 @@ prob = cec2015.CEC15Problems(prefix);
 
 
 
-for runtime = 1:5
+for runtime = 1:1
     % specify runtime for prob
     setCurrentRun(prob, runtime);
     for dimension = [10,30]    
@@ -22,25 +22,15 @@ for runtime = 1:5
         
         % recording rules are dimension related
        
-        for func_num = 1:15
-            if dimension == 2 && ...
-                    ((func_num >= 17 && func_num <= 22) ||...
-                    (func_num >= 29 && func_num <= 30))
-                continue;
-            end
-            
-            
+        for func_num = 1:1         
             
             
             x0 = -100+200*rand(dimension,1);
             
-            % prob.eval(x, dimesnion, numberofx, function_number)
-            % several evaluations could be make in the same time with
-            % numberofx > 1, x must be array with length
-            % dimension*numberofx
-            [x,y] = patternsearch(@(x)(eval(prob,x,dimension,1,func_num)),x0,[],[],[],[],...
+            func = @(x)(eval(prob,x,dimension,1,func_num));
+            [x,y] = patternsearch(func,x0,[],[],[],[],...
                 -100*ones(dimension,1),100*ones(dimension,1),[],...
-                psoptimset('display','off','MaxFunEvals',500*dimension));
+                psoptimset('display','iter','MaxFunEvals',500*dimension));
             
         end
         
--- src/io/IOUtils.java
+++ src/io/IOUtils.java
@@ -16,38 +16,60 @@ import java.io.PrintStream;
  * @author QIN
  */
 public class IOUtils {
+    private static boolean createDir(String f) {
+        if (null == f)
+            return true;
+        if (new File(f).exists())
+            return true;
+        else {
+            createDir(new File(f).getParent());
+            return new File(f).mkdir(); 
+        }  
+    }
     public static void flush(String filename, String contents) {
         try {
-            String fn      = filename;
-            String dirname = null;
-
-            if (fn.contains("/")) {
-                int slash = fn.lastIndexOf('/');
-
-                dirname = fn.substring(0, slash);
+            if (!createDir(new File(filename).getParent())) {
+                
+                System.out.println("Can't make directory.");
+                return;
             }
+//            String fn      = filename;
+//     
+//            
+//            
+//            
+//            
+//            String dirname = null;
+//
+//            if (fn.contains("/")) {
+//                int slash = fn.lastIndexOf('/');
+//
+//                dirname = fn.substring(0, slash);
+//            }
+//
+//            if (fn.contains("\\")) {
+//                int slash = fn.lastIndexOf('\\');
+//
+//                dirname = fn.substring(0, slash);
+//            }
+//
+//            if (dirname != null) {
+//                File dir = new File(dirname);
+//
+//                if (!dir.exists()) {
+//                    System.out.print("Create " + dir);
+//                    dir.mkdirs();
+//                    System.out.println(dir.exists()? " done." : " failed.");
+//                }
+//            }
 
-            if (fn.contains("\\")) {
-                int slash = fn.lastIndexOf('\\');
-
-                dirname = fn.substring(0, slash);
-            }
-
-            if (dirname != null) {
-                File dir = new File(dirname);
-
-                if (!dir.exists()) {
-                    dir.mkdirs();
-                }
-            }
-
-            File        f   = new File(fn);
+            File        f   = new File(filename);
             PrintStream out = new PrintStream(new FileOutputStream(f));
-
             out.print(contents);
             out.close();
         } catch (FileNotFoundException ex) {
             System.out.println("File Not Found when open output file.");
+            System.out.println("Check :" + filename);
         }
     }    
 }
